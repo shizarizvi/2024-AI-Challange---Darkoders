d432656aec4b1dd1778f20355492e12aa82e83fe
Syeda Shiza Rizvi <shiza07rizvi@gmail.com>
2024-04-17 00:32:02 +0500

    SVC_CHATBOT added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Career_Chatbot
+ # Career_Chatbot
  Your personal AI based career-counsellor chatbot that answers all your career-related queries. It is specifically trained to cater the needs of Pakistani students.
+ 
+ As a rough rule of thumb, 1 token is approximately 4 characters or 0.75 words for English text.
+
+ # to run
+ source .venv/bin/activate
+ pip install -r requirements.txt
+ python RAG_test.py
+
+ # activate service by entering this into your terminal:
+uvicorn main:app --reload
+
+# add the following prefix to your link on the browser:
+/chatbot/docs
---
+++ api/chatbot_response.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import constants
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException, Form, Query
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+class Request(BaseModel):
+    user_query: str = None
+    
+router = APIRouter()
+
+@router.get("/")
+async def health():
+    print("chatbot.api.health")
+    return {"message": "svc_chatbot is alive"}
+
+@router.post("/get_chatbot_response")
+async def create(request: Request):
+    user_query = request.user_query
+    chatbot_response = chatbot(user_query)
+    return {"chatbot_response": chatbot_response}
+
+
+def chatbot(user_query):
+    # Load the document, split it into chunks, embed each chunk and load it into the vector store.
+    data = TextLoader('data.txt').load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    split_data = text_splitter.split_documents(data)
+    db = Chroma.from_documents(split_data, OpenAIEmbeddings())
+
+    query = user_query
+    response = db.similarity_search(query)
+    return response[0].page_content
+
