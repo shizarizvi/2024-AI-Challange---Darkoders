c792dc14f8a390a8d85143b5953112fcb6d2eace
Syeda Shiza Rizvi <shiza07rizvi@gmail.com>
2024-05-01 17:20:01 +0500

    entire thing works with data file

--- api/chatbot_response.py
+++ api/chatbot_response.py
@@ -3,16 +3,23 @@ import sys
 import constants
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from fastapi import APIRouter, HTTPException, Form, Query
+from fastapi import APIRouter
 from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 class Request(BaseModel):
     user_query: str 
     
@@ -32,12 +39,33 @@ async def create(request: Request):
 
 def chatbot(user_query):
     # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-    data = TextLoader('data.txt').load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    split_data = text_splitter.split_documents(data)
-    db = Chroma.from_documents(split_data, OpenAIEmbeddings())
+    model = ChatOpenAI(api_key=OPENAI_API_KEY, model="gpt-3.5-turbo")
+    parser = StrOutputParser()
+    template = """
+    Answer the question based on the context below. If you can't 
+    answer the question, reply "I don't know.".
 
-    query = user_query
-    response = db.similarity_search(query)
-    return response[0].page_content
+    Context: {context}
 
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    loader = TextLoader("data/Research_ Chatbot Content.txt")
+    text_documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    data = text_splitter.split_documents(text_documents)
+    embeddings = OpenAIEmbeddings()
+    index_name = "the-musheer-app"
+    pinecone = PineconeVectorStore.from_documents(
+        data, embeddings, index_name=index_name
+    )
+    chain = (
+        {"context": pinecone.as_retriever(), "question": RunnablePassthrough()}
+        | prompt
+        | model
+        | parser
+    )
+    try:
+        return chain.invoke(user_query)
+    except Exception as e:
+        return e
